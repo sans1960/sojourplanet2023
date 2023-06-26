30aa42c1e5e6084a3038ea40de425a760a1f65eb
sans1960 <asanscliment@gmail.com>
2023-06-26 17:20:39 +0200

    lightbox tour

--- resources/views/front/imagestour.blade.php
+++ resources/views/front/imagestour.blade.php
@@ -10,6 +10,7 @@
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/tipo.css') }}">
     <link rel="stylesheet" href="{{ asset('css/front.css') }}">
+    <link rel="stylesheet" href="{{asset('css/lightbox.css')}}">
     {{-- <link rel="stylesheet" href="{{ asset('css/leaflet.css')}}"> --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
 </head>
@@ -21,15 +22,16 @@
         <div class="row mt-5">
             @foreach ($tour->imagetour as $item)
             <div class="col-md-4 mb-3">
-                <a href="{{ Storage::url($item->image) }}" data-toggle="lightbox"
-                    data-gallery="example-gallery" class="col-sm-4"
-                    data-caption="{{ $item->title }}">
+                <a href="{{ Storage::url($item->image) }}"
+                    data-lightbox="example-set" class="col-sm-4"
+                    data-title="{{ $item->title }}">
                     <img src="{{ Storage::url($item->image) }}" class="img-fluid">
                 </a>
             </div>
         @endforeach
         </div>
     </div>
+
     <div class="container-fluid bg-dark p-3">
         <h3 class="patua text-white text-center ">Image Gallery</h3>
     </div>
@@ -38,6 +40,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
 </script>
-<script src="https://cdn.jsdelivr.net/npm/bs5-lightbox@1.8.3/dist/index.bundle.min.js"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/bs5-lightbox@1.8.3/dist/index.bundle.min.js"></script> --}}
+
+    <script src="{{ asset('js/lightbox.js') }}"></script>
+
 </body>
 </html>
